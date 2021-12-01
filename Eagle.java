171c682b9b022c030561fa11955905137a70e095
Vianney Accart <vianneyaccart@gmail.com>
2021-12-01 16:59:35 +0100

    execute exercise

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly{
 
     private boolean flying;
     private int altitude;
@@ -8,6 +8,30 @@ public class Eagle extends Bird {
         this.flying = false;
         this.altitude = 0;
     }
+    
+  	public void takeOff() {
+  		System.out.println(getName() + " takes off in the sky.");
+  	};
+  	
+  	public int ascend(int meters) {
+  		this.altitude += meters;
+  		System.out.println(getName() + " flies upward, altitude : " + getAltitude());
+  		return meters;
+  	};
+  	
+  	public int descend(int meters){
+  		this.altitude -= meters;
+  		System.out.println(getName() + " flies downward, altitude : " + getAltitude());
+  		return meters;
+  	};
+  	
+  	public void land() {
+  		if (this.altitude > 1) {
+  			System.out.println(getName() + " is to high, it can't land.");
+  		} else {
+  			System.out.println(getName() + " lands on the ground");
+  		}
+  	};
 
     public int getAltitude() {
         return altitude;
